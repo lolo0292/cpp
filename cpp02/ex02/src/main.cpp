1993363fc542bf363d71f28e5f2bb715d9817fb7
Laurent Leichtnam <leichtnam2@gmail.com>
2025-12-30 11:31:48 +0100

    lolo last push

--- cpp02/ex02/src/main.cpp
+++ cpp02/ex02/src/main.cpp
@@ -5,14 +5,14 @@ int main() {
     Fixed a;
     Fixed const b(Fixed(5.05f) * Fixed(2));
 
-    std::cout << a << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
+    std::cout << a << std::endl; // affiche a
+    std::cout << ++a << std::endl; // affiche a + 1/256
+    std::cout << a << std::endl; // same car deja incrementer
+    std::cout << a++ << std::endl; // retourne la valeur prece puis incr
+    std::cout << a << std::endl; // affiche la valeur post incr
 
-    std::cout << b << std::endl;
-    std::cout << Fixed::max(a, b) << std::endl;
+    std::cout << b << std::endl; // affiche b 5.05*2
+    std::cout << Fixed::max(a, b) << std::endl; // affiche la plus grd entre a et b 
 
     return 0;
 }

d2aa1e4b5c6b871287853da15003a212614f5523
Laurent Leichtnam <leichtnam2@gmail.com>
2025-12-30 11:32:00 +0100

    lolo last push

--- cpp02/ex00/src/Fixed.cpp
+++ cpp02/ex00/src/Fixed.cpp
@@ -2,35 +2,43 @@
 
 const int Fixed::_fracBits = 8;
 
-// Default constructor
-Fixed::Fixed() : _raw(0) {
+// init raw a 0
+Fixed::Fixed() : _raw(0) 
+{
     std::cout << "Default constructor called" << std::endl;
 }
 
-// Copy constructor
-Fixed::Fixed(const Fixed &other) : _raw(other._raw) {
+// Copy constructor de raw depuis other
+Fixed::Fixed(const Fixed &other) : _raw(other._raw) 
+{
     std::cout << "Copy constructor called" << std::endl;
 }
 
 // Copy assignment operator
-Fixed &Fixed::operator=(const Fixed &other) {
+Fixed &Fixed::operator=(const Fixed &other) 
+{
     std::cout << "Copy assignment operator called" << std::endl;
     if (this != &other)
         this->_raw = other._raw;
     return *this;
 }
 
-// Destructor
-Fixed::~Fixed() {
+// Destructeur call a la fin
+Fixed::~Fixed() 
+{
     std::cout << "Destructor called" << std::endl;
 }
 
-int Fixed::getRawBits() const {
+// put mess et return rAW
+int Fixed::getRawBits() const 
+{
     std::cout << "getRawBits member function called" << std::endl;
     return this->_raw;
 }
 
-void Fixed::setRawBits(int const raw) {
+//useless ici mais oblige et utile 0102
+void Fixed::setRawBits(int const raw) 
+{
     std::cout << "setRawBits member function called" << std::endl;
     this->_raw = raw;
 }
